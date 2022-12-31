4c568112474cc33789b17178a1c74f70978f7e51
mukul47 <mukuldutt1234@gmail.com>
2022-12-31 06:35:16 +0530

    added cors

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ import http from "http";
 import express from "express";
 import path from "path";
 import socket from "socket.io";
+import cors from "cors";
 const app = express();
 app.use(express.static(path.join(__dirname, "..", "view_build")));
 app.get("/", async (req: any, res: any, next: any) =>
   res.sendFile(path.join(__dirname, "..", "view_build", "index.html"))
 );
+app.use(cors({ origin: "*" }));
 const server = http.createServer(app);
 new EventService(new socket.Server(server, {}));
-server.listen(process.env.PORT || 8080);
+server.listen(process.env.PORT || "8080", () => {
+  console.log(`STARTED AT ${process.env.PORT || "8080"}`);
+});
