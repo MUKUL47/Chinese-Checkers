30491218942964c652d84b0a9048f2e36afc96c6
mukul47 <mukuldutt1234@gmail.com>
2022-12-31 07:10:01 +0530

    test path

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,15 @@ import path from "path";
 import socket from "socket.io";
 import cors from "cors";
 const app = express();
+app.use(cors({ origin: "*" }));
+const server = http.createServer(app);
+new EventService(new socket.Server(server, {}));
 app.use(express.static(path.join(__dirname, "..", "view_build")));
 app.get("/", async (req: any, res: any, next: any) =>
   res.sendFile(path.join(__dirname, "..", "view_build", "index.html"))
 );
-app.use(cors({ origin: "*" }));
-const server = http.createServer(app);
-new EventService(new socket.Server(server, {}));
+app.get("/test", async (req: any, res: any, next: any) => res.send("test"));
 server.listen(process.env.PORT || "8080", () => {
   console.log(`STARTED AT ${process.env.PORT || "8080"}`);
+  console.log(`STARTED AT ${JSON.stringify(process.env, null, 1) || "8080"}`);
 });
